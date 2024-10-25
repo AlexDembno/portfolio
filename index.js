0a9ea553846bdf62a67fe7f939613e35966a4e70
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2024-10-25 12:03:40 +0200

    add gradient

--- index.js
+++ index.js
@@ -16,3 +16,38 @@ document.addEventListener("DOMContentLoaded", function () {
 		observer.observe(target);
 	});
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+	const observer = new IntersectionObserver(
+		entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("visible");
+				}
+			});
+		},
+		// { threshold: 1.0 }
+	); // 100% элемента должно быть видно
+
+	const targets = document.querySelectorAll(".animated_text");
+
+	targets.forEach(target => {
+		observer.observe(target);
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const paragraphs = document.querySelectorAll(".animated_title");
+
+	paragraphs.forEach(paragraph => {
+		const text = paragraph.innerText;
+		paragraph.innerText = ""; // очищаем текст
+
+		text.split("").forEach((char, index) => {
+			const span = document.createElement("span");
+			span.textContent = char === " " ? "\u00A0" : char; // сохраняем пробелы
+			span.style.animationDelay = `${index * 50}ms`; // задержка 50ms на каждый символ
+			paragraph.appendChild(span);
+		});
+	});
+});
