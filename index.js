2bb552215c4b9ba3f2615465e05848ead39cb3de
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2024-12-07 13:50:13 +0100

    add law company

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const observer = new IntersectionObserver(
-		entries => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					entry.target.classList.add("visible");
-				}
-			});
-		},
-		// { threshold: 1.0 }
-	); // 100% элемента должно быть видно
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("visible");
+			}
+		});
+	});
 
 	const targets = document.querySelectorAll(".animated_element");
 
@@ -18,16 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-	const observer = new IntersectionObserver(
-		entries => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					entry.target.classList.add("visible");
-				}
-			});
-		},
-		// { threshold: 1.0 }
-	); // 100% элемента должно быть видно
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("visible");
+			}
+		});
+	});
 
 	const targets = document.querySelectorAll(".animated_text");
 
@@ -41,12 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	paragraphs.forEach(paragraph => {
 		const text = paragraph.innerText;
-		paragraph.innerText = ""; // очищаем текст
+		paragraph.innerText = "";
 
 		text.split("").forEach((char, index) => {
 			const span = document.createElement("span");
-			span.textContent = char === " " ? "\u00A0" : char; // сохраняем пробелы
-			span.style.animationDelay = `${index * 50}ms`; // задержка 50ms на каждый символ
+			span.textContent = char === " " ? "\u00A0" : char;
+			span.style.animationDelay = `${index * 50}ms`;
 			paragraph.appendChild(span);
 		});
 	});
