c998f81f1d93a30124b49b13a69c443e572dce1b
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2025-02-26 19:41:54 +0100

    update

--- index.js
+++ index.js
@@ -45,3 +45,47 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	});
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+	const refs = {
+		list: document.querySelector(".form"),
+		formSend: document.querySelector(".submit"),
+		text: document.querySelector(".sign-text"),
+	};
+
+	if (!refs.list) return;
+
+	refs.list.addEventListener("submit", event => {
+		event.preventDefault();
+
+		refs.formSend.disabled = true;
+
+		const formData = new FormData(refs.list);
+		const data = {
+			name: formData.get("name"),
+			phone: formData.get("telefon"),
+			message: formData.get("textarea"),
+		};
+
+		setTimeout(() => {
+			refs.text.innerHTML = `Message sent. We'll contact you soon.`;
+		}, 1500);
+
+		fetch("https://lawcompanyback.onrender.com/send", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error("Ошибка отправки данных");
+				}
+				return response.text();
+			})
+			.catch(error => {
+				console.error("Ошибка:", error);
+			});
+	});
+});
